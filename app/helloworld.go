4baebb7495f796a6af287e6831973e4e590a406f
Johan Euphrosine <proppy@google.com>
2014-05-30 16:15:00 -0700

    app: remove init

--- app/helloworld.go
+++ app/helloworld.go
@@ -6,14 +6,11 @@ import (
 	"net/http"
 )
 
-func init() {
-	http.HandleFunc("/", handle)
-}
-
 func handle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<html><body>Hello, World! 세상아 안녕!</body></html>")
 }
 
 func main() {
+	http.HandleFunc("/", handle)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
