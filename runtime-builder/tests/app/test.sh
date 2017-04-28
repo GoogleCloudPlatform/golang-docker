0095b006fc18cd3bdc1164a303ec68f4fa089c9b
Tuo Shan <sturbo89@gmail.com>
2017-04-28 12:46:34 -0700

    Add checks to the runtime builders related configs

--- runtime-builder/tests/app/test.sh
+++ runtime-builder/tests/app/test.sh
@@ -26,5 +26,13 @@ if [[ -z "${PROJECT}" ]]; then
     usage
 fi
 
+# Check if the config file is set to the proper local path
+use_rb="$(gcloud config -q get-value app/use_runtime_builders)"
+rb_root="$(gcloud config -q get-value app/runtime_builders_root)"
+if [[ "${use_rb}" = "False" || "${rb_root}" != file://* ]]; then
+    echo "Wrong gcloud config found for app/use_runtime_builders or app/runtime_builders_root."
+    exit 1
+fi
+
 gcloud beta app deploy --project="${PROJECT}" app.yaml && \
 gcloud beta container builds submit --project="${PROJECT}" --config=test.yaml .
