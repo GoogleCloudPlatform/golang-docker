29571b6fd8f410edc3d8198f0577215271bf46fe
shantuo <sturbo89@gmail.com>
2017-10-31 10:12:05 -0700

    runtime-builder/tests: use vendor directory for dependencies (#120)
    * use vendor directory for dependencies
    
    * update the go-get script to use vendor directory

--- runtime-builder/tests/integration/go-get.sh
+++ runtime-builder/tests/integration/go-get.sh
@@ -5,26 +5,25 @@ set -euo pipefail
 # Set GOPATH to a temporary directory for downloading external dependencies.
 export GOPATH="$(mktemp -d /tmp/gopath.XXX)"
 
-# Set new_gopath to this directory.
-cd $(dirname $0)
-new_gopath=$(pwd -P)
-
 echo "Cleaning up dependencies..."
-find src/ -maxdepth 1 -mindepth 1 ! -name 'app'  -type d | xargs rm -rf
+cd $(dirname $0)
+rm -rf src/app/vendor
 
 echo "Fetching dependencies..."
 cd src/app
-go get -v -d -tags appenginevm
+vendor_dir="$(pwd -P)/vendor"
+mkdir "${vendor_dir}"
+go get -d -tags appenginevm
 deps=$(go list -f '{{ join .Deps "\n" }}' -tags appenginevm)
 
 echo "Copying dependencies..."
 for pkg in ${deps}
 do
     d="${GOPATH}/src/${pkg}"
-    # Copy external dependencies to new_gopath.
+    # Copy external dependencies to vendor_dir.
     if [[ -d "${d}" ]]; then
-        mkdir -p "${new_gopath}/src/${pkg}"
-        set +e; cp -v "${GOPATH}/src/${pkg}"/*.go "${new_gopath}/src/${pkg}/"
+        mkdir -p "${vendor_dir}/${pkg}"
+        set +e; cp -v "${GOPATH}/src/${pkg}"/*.go "${vendor_dir}/${pkg}/"
     fi
 done
 
@@ -34,10 +33,10 @@ cd "${GOPATH}"
 # only. If this is not the case in the future, we should change this logic.
 for license in $(find . -name LICENSE)
 do
-    set +e; cp -v "${license}" "${new_gopath}/${license}"
+    set +e; cp -v "${license}" "${vendor_dir}/${license}"
 done
 
 rm -rf "${GOPATH}"
-cd "${new_gopath}"
+cd "${vendor_dir}"
 # Remove test files.
-find src/ -name \*_test.go -delete
+find . -name \*_test.go -delete
