db0af3e5c3a89df7c881d2034dd9516b7f149c01
jahjahbin <113399288+jahjahbin@users.noreply.github.com>
2023-08-08 07:56:22 -0700

    Update builder.sh

--- tools/ci-scripts/builder.sh
+++ tools/ci-scripts/builder.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 export KOKORO_GITHUB_DIR=${KOKORO_ROOT}/src/github
+
+# Help debug auth used by the pipelines.
+gcloud auth list
+
 source ${KOKORO_GFILE_DIR}/kokoro/common.sh
 
 cd ${KOKORO_GITHUB_DIR}/golang-docker/runtime-builder
